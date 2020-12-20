a31fd4762cac3b46e75cfe2c1e18a8f36f0ece5a
Steven Wong <sing1708@gmail.com>
2020-12-20 12:06:35 +0800

    add rspec and user test case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#FactoryBot" do
+    it 'can have factory' do
+      user = FactoryBot.create(:user)
+      expect(user).to be_a User
+      expect(user.id).to be_truthy
+    end
+  end
+
+  describe "#sign_in" do
+    it 'can sign in' do
+      user = FactoryBot.create(:user, password: "password1", password_confirmation: "password1")
+      expect(user.valid_password?('password1')).to be_truthy
+    end
+  end
+
+  describe "#sign_up" do
+    it 'email need to be unique' do
+      FactoryBot.create(:user, email: "example@example.com")
+      user = User.new(
+        email: 'example@example.com',
+        password: '1234Qwer!',
+        password_confirmation: '1234Qwer!'
+      )
+      expect(user.save).to be_falsey
+    end
+  end
+
+
 end
